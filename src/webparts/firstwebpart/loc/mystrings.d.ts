fddda3602257adb8a42996792168b29e938e1df1
jibinkoshy <jibinkoshy@hotmail.com>
2020-10-11 08:11:22 -0500

    first commit

---
+++ src/webparts/firstwebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFirstwebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FirstwebpartWebPartStrings' {
+  const strings: IFirstwebpartWebPartStrings;
+  export = strings;
+}
